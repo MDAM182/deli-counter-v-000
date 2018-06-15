e831fd913d00c3dd1a3147c35bff5e290719d6bd
Michael Morris <mdam2345@gmail.com>
2018-06-15 01:09:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 def line(katz_deli)
-  if katz_deli == 0 
+  if katz_deli == 0
     puts "The line is currently empty."
   elsif katz_deli == num
     puts "#{katz_deli}"
-    
+  else false
+  end 
 end
